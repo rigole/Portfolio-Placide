9a1cc57e958ab89d02052246157bc54f8ad19579
rigole <foplacide@gmail.com>
2021-09-03 22:08:11 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,235 @@
+$(document).ready(function() {
+    "use strict";
+
+    // Nav Menu
+    $(document).on('click', '.nav-menu a, .mobile-nav a', function(e) {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var hash = this.hash;
+            var target = $(hash);
+            if (target.length) {
+                /*e.preventDefault();*/
+
+                if ($(this).parents('.nav-menu, .mobile-nav').length) {
+                    $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+                    $(this).closest('li').addClass('active');
+                }
+
+                if (hash == '#header') {
+                    $('#header').removeClass('header-top');
+                    $("section").removeClass('section-show');
+                    if ($('body').hasClass('mobile-nav-active')) {
+                        $('body').removeClass('mobile-nav-active');
+                        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+                        $('.mobile-nav-overly').fadeOut();
+                    }
+                    return;
+                }
+
+                if (!$('#header').hasClass('header-top')) {
+                    $('#header').addClass('header-top');
+                    setTimeout(function() {
+                        $("section").removeClass('section-show');
+                        $(hash).addClass('section-show');
+
+                    }, 350);
+                } else {
+                    $("section").removeClass('section-show');
+                    $(hash).addClass('section-show');
+                }
+
+                $('html, body').animate({
+                    scrollTop: 0
+                }, 350);
+
+                if ($('body').hasClass('mobile-nav-active')) {
+                    $('body').removeClass('mobile-nav-active');
+                    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+                    $('.mobile-nav-overly').fadeOut();
+                }
+
+                return false;
+
+            }
+        }
+    });
+
+    // Activate/show sections on load with hash links
+    if (window.location.hash) {
+        var initial_nav = window.location.hash;
+        if ($(initial_nav).length) {
+            $('#header').addClass('header-top');
+            $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+            $('.nav-menu, .mobile-nav').find('a[href="' + initial_nav + '"]').parent('li').addClass('active');
+            setTimeout(function() {
+                $("section").removeClass('section-show');
+                $(initial_nav).addClass('section-show');
+            }, 350);
+        }
+    }
+
+    // Mobile Navigation
+    if ($('.nav-menu').length) {
+        var $mobile_nav = $('.nav-menu').clone().prop({
+            class: 'mobile-nav d-lg-none'
+        });
+        $('body').append($mobile_nav);
+        $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+        $('body').append('<div class="mobile-nav-overly"></div>');
+
+        $(document).on('click', '.mobile-nav-toggle', function(e) {
+            $('body').toggleClass('mobile-nav-active');
+            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+            $('.mobile-nav-overly').toggle();
+        });
+
+        $(document).click(function(e) {
+            var container = $(".mobile-nav, .mobile-nav-toggle");
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
+                if ($('body').hasClass('mobile-nav-active')) {
+                    $('body').removeClass('mobile-nav-active');
+                    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+                    $('.mobile-nav-overly').fadeOut();
+                }
+            }
+        });
+    } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+        $(".mobile-nav, .mobile-nav-toggle").hide();
+    }
+
+    // jQuery counterUp
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+    // Skills section
+    $('.skills-content').waypoint(function() {
+        $('.progress .progress-bar').each(function() {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {
+        offset: '80%'
+    });
+
+    // Testimonials carousel (uses the Owl Carousel library)
+    $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            900: {
+                items: 3
+            }
+        }
+    });
+
+    // Porfolio isotope and filter
+    $(window).on('load', function() {
+        var portfolioIsotope = $('.portfolio-container').isotope({
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
+        });
+
+        $('#portfolio-flters li').on('click', function() {
+            $("#portfolio-flters li").removeClass('filter-active');
+            $(this).addClass('filter-active');
+
+            portfolioIsotope.isotope({
+                filter: $(this).data('filter')
+            });
+        });
+
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+        $('.venobox').venobox({
+            'share': false
+        });
+    });
+
+    // Portfolio details carousel
+    $(".portfolio-details-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+
+    // Typing effect
+
+    // List of title
+    const _content = [
+        "FullStack Web Developper",
+        "Software Engineer",
+        "Data Scientist",
+        "Machine Learning Engineer"
+    ]
+
+    // Current sentence being processed
+    let _Part = 0;
+
+    // Character number of the current sentence being processed
+    let _part_index = 0;
+
+    //Holds the handle returned from setInterval
+    let _interval_val;
+
+    // Element that holds the text
+    let _element = document.querySelector("#text");
+
+    // Cursor element
+    const _cursor = document.querySelector("#cursor");
+
+    function Typing() {
+        let text = _content[_Part].substring(0, _part_index + 1);
+        _element.innerHTML = text;
+        _part_index++;
+
+        // If full sentence has been displayed then start to delete the sentence after some time
+        if (text === _content[_Part]){
+            // Hide the cursor
+            _cursor.style.display = 'none';
+            clearInterval(_interval_val);
+            setTimeout(function () {
+                _interval_val = setInterval(Delete, 50)
+            }, 1000);
+        }
+    }
+
+    // Implements deleting effect
+
+    function Delete() {
+        //  Get substring with 1 characater deleted
+        let text = _content[_Part].substring(0, _part_index - 1);
+        _element.innerHTML = text;
+        _part_index--;
+
+        // If sentence has been deleted then start to display the next sentence
+        if (text === '') {
+            clearInterval(_interval_val);
+
+            // If current sentence was last then display the first one, else move to the next
+            if (_Part == (_content.length - 1 )){
+                _Part = 0;
+            }
+            else {
+                _Part++;
+            }
+            _part_index = 0;
+
+            // Start to display the next sentence after some time
+            setTimeout(function () {
+                _cursor.style.display = 'inline-block';
+                _interval_val = setInterval(Typing, 100);
+            }, 200)
+        }
+    }
+    _interval_val=setInterval(Typing, 100);
+});
